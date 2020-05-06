a4b8d5e92c6673d1c8705d09aebe6624d73b94f8
mbaskakova <mbaskakova@4xxi.com>
2020-05-06 18:07:07 +0300

    add cors support, add logger

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Baldinof\RoadRunnerBundle\BaldinofRoadRunnerBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
--- src/Event/ErrorResponseListener.php
+++ src/Event/ErrorResponseListener.php
@@ -4,17 +4,40 @@ declare(strict_types=1);
 
 namespace App\Event;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Throwable;
 
 class ErrorResponseListener
 {
+    /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
     public function onKernelException(ExceptionEvent $event)
     {
         $exception = $event->getThrowable();
 
+        $this->logError($exception);
+
         $response = new JsonResponse(['message' => $exception->getMessage()], $exception->getStatusCode());
         $event->setResponse($response);
+    }
+
+    private function logError(Throwable $exception)
+    {
+        $context = [
+            'code' => $exception->getCode(),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+        ];
+        $this->logger->error($exception->getMessage(), $context);
     }
 }
